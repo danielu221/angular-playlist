3f341adbd40d8c26711b45528e7ebaa2c844823e
Student <danielukmateusz@gmail.com>
2018-07-20 14:11:20 +0200

    use switchMap

--- src/app/music/music-search/music-search.component.ts
+++ src/app/music/music-search/music-search.component.ts
@@ -17,7 +17,7 @@ export class MusicSearchComponent implements OnInit {
 
   ngOnInit() {
    this.albums = this.musicService.getAlbums()
-    // .subscribe((albums)=>{this.albums=albums}),
+    // .subscribe((albums)=>{this.albums = albums}),
     // err => {
     //   console.log(err)
     // };
--- src/app/music/music.service.ts
+++ src/app/music/music.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { SecurityService } from '../security/security.service';
 
 export const SEARCH_URL = new InjectionToken("URL for albums search API");
-import {map, catchError} from "rxjs/operators"
+import {map, catchError, switchMap} from "rxjs/operators"
 import { throwError, Subject, of } from '../../../node_modules/rxjs';
 
 interface AlbumsResponse{
@@ -19,44 +19,76 @@ interface AlbumsResponse{
 })
 export class MusicService {
 
-  constructor(@Inject(SEARCH_URL) private api_url, private http:HttpClient, private security:SecurityService) { }
-
   queries = new Subject<string>()
   albums = new Subject<Album[]>()
+
+  constructor(
+    @Inject(SEARCH_URL) private api_url, 
+  private http:HttpClient, 
+  private security:SecurityService) {
+    this.queries.pipe(
+     map(query=>({
+        type:'album',
+        q: query
+      })),
+      switchMap(params => this.http.get<AlbumsResponse>(this.api_url,{
+       headers:{
+         Authorization:'Bearer ' + this.security.getToken()
+       },
+       params
+     })),
+     map((response)=>{
+           return response.albums.items;
+         }),
+         catchError((err) => {
+           if(err instanceof HttpErrorResponse){
+           if(err.status == 401){
+             this.security.authorize()
+             return throwError(new Error("Access denied!"))
+           }
+           }
+           return of([])
+         })
+     )
+     .subscribe(albums=>{
+       this.albums.next(albums)
+     });
+   }
+
 
   search(query){
     this.queries.next(query)
   }
 
   getAlbums(){
-    this.queries.subscribe(query=>{
-       this.http.get<AlbumsResponse>(this.api_url,{
-        headers:{
-          Authorization:'Bearer ' + this.security.getToken()
-        },
-        params:{
-          type:'album',
-          q: query
-        }
-      })
-      .pipe(
-          map((response)=>{
-            return response.albums.items;
-          }),
-          catchError((err) => {
-            if(err instanceof HttpErrorResponse){
-            if(err.status == 401){
-              this.security.authorize()
-              return throwError(new Error("Access denied!"))
-            }
-            }
-            return of([])
-          })
-      )
-      .subscribe(albums=>{
-        this.albums.next(albums)
-      });
-    })
+    // this.queries.subscribe(query=>{
+    //    this.http.get<AlbumsResponse>(this.api_url,{
+    //     headers:{
+    //       Authorization:'Bearer ' + this.security.getToken()
+    //     },
+    //     params:{
+    //       type:'album',
+    //       q: query
+    //     }
+    //   })
+    //   .pipe(
+    //       map((response)=>{
+    //         return response.albums.items;
+    //       }),
+    //       catchError((err) => {
+    //         if(err instanceof HttpErrorResponse){
+    //         if(err.status == 401){
+    //           this.security.authorize()
+    //           return throwError(new Error("Access denied!"))
+    //         }
+    //         }
+    //         return of([])
+    //       })
+    //   )
+    //   .subscribe(albums=>{
+    //     this.albums.next(albums)
+    //   });
+    // })
     return this.albums;
    
   }
